d19228d9cdf90a5e8b37ce57c1bb64f83205e6ff
dev.juyoung <dev.juyoung@gmail.com>
2017-04-15 16:09:09 +0900

    * added method: Generate commonly available toast message methods

--- cropicker/src/main/java/xyz/dev_juyoung/cropicker/base/BaseActivity.java
+++ cropicker/src/main/java/xyz/dev_juyoung/cropicker/base/BaseActivity.java
@@ -2,8 +2,11 @@ package xyz.dev_juyoung.cropicker.base;
 
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 
@@ -21,5 +24,13 @@ public class BaseActivity extends AppCompatActivity {
     public void setContentView(@LayoutRes int layoutResID) {
         super.setContentView(layoutResID);
         ButterKnife.bind(this);
+    }
+
+    public void showMessage(@StringRes int resId) {
+        showMessage(getString(resId));
+    }
+
+    public void showMessage(@NonNull String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
